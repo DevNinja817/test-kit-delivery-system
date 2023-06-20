0913ae36d05a678f312d64f52068f7f922503449
regX1 <98269264+regX1@users.noreply.github.com>
2023-06-20 23:00:12 +0100

    Updated Volunteer.java
    Added an option for a volunteer to be a leader.

--- Volunteer.java
+++ Volunteer.java
@@ -1,60 +1,73 @@
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class Volunteer implements Comparable<Volunteer>{
+public class Volunteer implements Comparable<Volunteer> {
 
     String name;
     int test_kits;
+    boolean isLeader;
 
-    public Volunteer(String name, int test_kits) {
+    public Volunteer(String name, int test_kits, boolean isLeader) {
         this.name = name;
         this.test_kits = test_kits;
+        this.isLeader = isLeader;
     }
 
-    public static String formattedCurrency(double amount){
+    public static String formattedCurrency(double amount) {
         NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.UK);
         return currency.format(amount);
     }
 
-    //Getter methods to get these values from another class in same package
-    public String getName(){return name;}
-    public int getTest_kit(){return test_kits;}
+    // Getter methods to get these values from another class in same package
+    public String getName() {
+        return name;
+    }
+
+    public int getTest_kit() {
+        return test_kits;
+    }
+
+    public boolean getisLeader() {
+        return isLeader;
+    }
 
     public void extraKits(int extra_kits) throws Exception {
-        if(extra_kits <= 0) {
+        if (extra_kits <= 0) {
             throw new Exception("You can't deliver less than 0 kits!");
-        }
-        else{
+        } else {
             test_kits += extra_kits;
         }
     }
 
-    //Calculate Volunteers(Objects) wages based off their given constructor values.
+    // Calculate Volunteers(Objects) wages based off their given constructor values.
     public double calculateWages() {
         double volunteer_wages = 0;
 
-        //This calculates price if test kit amount is over 50.
-        if (test_kits > 50){
+        // This calculates price if test kit amount is over 50.
+        if (test_kits > 50) {
             volunteer_wages = volunteer_wages + 50 * 0.15;
             int minusFiftyKits = test_kits - 50;
 
             volunteer_wages += minusFiftyKits * 0.20;
-        }
-        else
-        {
-            //This calculates price if test kit amount is less than 50.
+        } else {
+            // This calculates price if test kit amount is less than 50.
             volunteer_wages = test_kits * 0.15;
+        }
+        // Adds 20% leader bonus if the employee is a leader
+        if (isLeader) {
+            volunteer_wages *= 1.2;
         }
         return volunteer_wages;
     }
 
     public static void main(String[] args) {
+        // Initialise currency object to format any double to UK currency (GBP)
         NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.UK);
 
-        Volunteer bob = new Volunteer("Bob", 123);
+        Volunteer bob = new Volunteer("Bob", 123, false);
         System.out.println(bob.name + "s wages are: " + currency.format(bob.calculateWages()));
 
-        Volunteer cal = new Volunteer("Cal", 53);
+        Volunteer cal = new Volunteer("Cal", 53, false);
         System.out.println(cal.name + "s wages are: " + currency.format(cal.calculateWages()));
 
     }
